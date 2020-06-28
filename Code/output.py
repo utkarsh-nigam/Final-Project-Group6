ebb634e238bb6141a4279ef81cde61d8749cb7b8
Utkarsh Nigam <contacts.utkarsh@gmail.com>
2020-06-28 12:12:38 -0400

    Update output.py

--- Code/output.py
+++ Code/output.py
@@ -15,5 +15,20 @@ test1=test.drop(columns=["Unnamed: 0","Label"])
 y_pred = clf.predict(test1)
 folders=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
+attempts=np.zeros(26,int)
+correct=np.zeros(26,int)
+
+attempt_dict=dict(zip(folders,attempts))
+correct_dict=dict(zip(folders,correct))
+
+
 for i in range(0,len(y_pred)):
     print("Actual: ",test["Label"].iloc[i]," Predicted: ", folders[y_pred[i]])
+    attempt_dict[test["Label"].iloc[i]]+=1
+    if(test["Label"].iloc[i]==folders[y_pred[i]]):
+        correct_dict[test["Label"].iloc[i]] += 1
+
+for key in attempt_dict:
+    if (attempt_dict[key]>0):
+        print("\nCharacter: ", key, "\tAttempts: ",attempt_dict[key], "\tCorrect: ",correct_dict[key], "\tAccuracy: ", round((correct_dict[key]/attempt_dict[key])*100,2))
+
